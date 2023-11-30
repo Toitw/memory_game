42d1c33ee9495bf3a4379cb180fb744ad2b5e4e5
juroga_new <juanjrgast@gmail.com>
2023-11-30 19:15:42 +0100

    Fix duplicate randomId bug in RandomPokemonGrid

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -8,6 +8,10 @@ function RandomPokemonGrid() {
             let fetchedPokemons = [];
             for (let i = 0; i < 12; i++) {
                 const randomId = Math.floor(Math.random() * 898) + 1;
+                if (fetchedPokemons.find(pokemon => pokemon.id === randomId)) {
+                    i--;
+                    continue;
+                }
                 const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
                 const data = await response.json();
                 fetchedPokemons.push(data);
